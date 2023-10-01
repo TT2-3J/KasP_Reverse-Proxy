35b5ab27db866614c465a7a26269e174764da573
kasidatep <bank@kasidate.me>
2023-10-01 21:12:06 +0700

    [ADD] Reverse Proxy For SAS Dev Environment

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+const app = express();
+
+// Create a proxy middleware for /api/ backend
+const apiProxy = createProxyMiddleware('/api', {
+  target: 'http://localhost:8080',
+  changeOrigin: false,
+});
+
+// Create a proxy middleware for / frontend
+const rootProxy = createProxyMiddleware('/', {
+  target: 'http://localhost:5173',
+  changeOrigin: false,
+});
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.use('/api', apiProxy);
+app.use('/', rootProxy);
+
+
+const port = 3000; 
+app.listen(port, () => {
+  console.log(`Reverse proxy server is running on port ${port}`);
+});
